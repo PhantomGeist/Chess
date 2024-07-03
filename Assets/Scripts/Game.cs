846711a7366a7bb9dc0b67d178f95ddae17dddc1
PhantomGeist <arya95514@gmail.com>
2024-07-03 15:13:55 +0700

    Move Plate
    Adding Move Plate

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -36,7 +36,7 @@ public class Game : MonoBehaviour
             Create("black_pawn", 7, 6)
         };
 
-        for (int i = 0; 1 < playerBlack.Length; i++)
+        for (int i = 0; i < playerBlack.Length; i++)
         {
             SetPosition(playerBlack[i]);
             SetPosition(playerWhite[i]);
@@ -60,4 +60,29 @@ public class Game : MonoBehaviour
 
         positions[cm.GetXBoard(), cm.GetYBoard()] = obj;
     }
+
+    public void SetPositionEmpty(int x, int y)
+    {
+        positions[x,y] = null;
+    }
+
+    public GameObject GetPosition(int x, int y)
+    {
+        return positions[x, y];
+    }
+
+    public bool PositionOnBoard(int x, int y)
+    {
+        if (x < 0 || y < 0 || x >= positions.GetLength(0) || y >= positions.GetLength(1))
+        {
+            return false;
+        }
+
+        else
+        {
+            return true;
+        }
+    }
+
+
 }
